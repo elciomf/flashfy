6f5c3028a463bb2b42dba01f2d16b51d9a98cb38
Élcio M. Fernandes <elciomf@proton.me>
2026-03-25 01:11:46 -0300

    feat: implement basic express server with health check

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const payload = {
+    status: "ok",
+  };
+
+  res.send(payload);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
