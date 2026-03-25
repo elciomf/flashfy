ffaa722cdd02d64127679a4c6f8843443f580b88
Élcio M. Fernandes <elciomf@proton.me>
2026-03-25 16:01:01 -0300

    fix: resolve ESM and dotenv bundle error on Render deployment

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   format: ["esm"],
+  target: "node22",
   clean: true,
-  minify: true,
+  minify: false,
   sourcemap: true,
+
+  external: [
+    "dotenv",
+    "dotenv/config",
+    "express",
+    "typeorm",
+    "pg",
+    "reflect-metadata",
+  ],
 });
