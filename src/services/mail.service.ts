61aa68fc45ec0c3f864003d95cd65afa6ad1025c
Élcio M. Fernandes <elciomf@proton.me>
2026-03-26 12:45:16 -0300

    feat: implement generic mail service using Resend

---
+++ src/services/mail.service.ts
@@ -0,0 +1,30 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async ({
+  to,
+  subject,
+  html,
+}: {
+  to: string;
+  subject: string;
+  html: string;
+}) => {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Flashfy <noreply@flashfy.xyz>",
+      to: [to],
+      subject,
+      html,
+    });
+
+    if (error) {
+      return { success: false, error };
+    }
+
+    return { success: true, data };
+  } catch (err) {
+    return { success: false, error: err };
+  }
+};
